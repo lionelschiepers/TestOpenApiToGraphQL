b4e9030695afd64cb6fad955186d2008e2248efb
Lionel Schiepers <lionel.schiepers@belgiantrain.be>
2020-06-16 21:45:32 +0200

    load the schema from internet
    load the schema from internet

--- index.js
+++ index.js
@@ -3,16 +3,31 @@ const graphqlHTTP = require('express-graphql')
 const { createGraphQLSchema } = require('openapi-to-graphql')
 const fs = require('fs');
 const util = require("util");
+const axios = require("axios");
 
 const readFile = util.promisify(fs.readFile);
 
-async function main() {
-    let oas = JSON.parse(await readFile('./pmr3.json'));
+const getData = async url => {
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    if (oas == null)
+async function main() {
+    // let oas = JSON.parse(await readFile('./pmr3.json'));
+    // let oas = JSON.parse('https://raw.githubusercontent.com/lionelschiepers/TestOpenApiToGraphQL/master/PMR3.json');
+
+    let oas = await getData('https://raw.githubusercontent.com/lionelschiepers/TestOpenApiToGraphQL/master/PMR3.json');
+    let oas2 = await getData('https://raw.githubusercontent.com/lionelschiepers/TestOpenApiToGraphQL/master/PMR3-2.json');
+
+    if (oas == null || oas2 == null)
         throw new Error('failed to load the file');
 
-    const { schema, report } = await createGraphQLSchema(oas, 
+    const { schema, report } = await createGraphQLSchema([oas, oas2], 
         {
             operationIdFieldNames:true,
             requestOptions:
