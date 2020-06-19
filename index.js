74e4054e0d0781f39273054dbd4e02ea5ac6869e
Lionel Schiepers <lionel.schiepers@belgiantrain.be>
2020-06-19 16:53:38 +0200

    use public openapi description

--- index.js
+++ index.js
@@ -20,11 +20,12 @@ const getData = async url => {
 async function main() {
     // links between schema use the title and not the address like defined in the openapi 3.0
     // https://www.npmjs.com/package/@mikestaub/openapi-to-graphql
-    let oas1 = JSON.parse(await readFile('PMR3.json'));
-    let oas2 = JSON.parse(await readFile('PMR3-2.json'));
+    // let oas1 = JSON.parse(await readFile('PMR3.json'));
+    // let oas2 = JSON.parse(await readFile('PMR3-2.json'));
     
-    // let oas1 = await getData('https://raw.githubusercontent.com/lionelschiepers/TestOpenApiToGraphQL/master/PMR3.json');
-    // let oas2 = await getData('https://raw.githubusercontent.com/lionelschiepers/TestOpenApiToGraphQL/master/PMR3-2.json');
+    
+    let oas1 = await getData('https://belgianrail-station.azurewebsites.net/openapi.json');
+    let oas2 = await getData('https://belgianrail-reservation.azurewebsites.net/openapi.json');
 
     if (oas1 == null || oas2 == null)
         throw new Error('failed to load the file');
